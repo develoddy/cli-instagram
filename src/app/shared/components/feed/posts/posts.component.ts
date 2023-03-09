cc5f3202b638a734ea9899f3a13bfdbcf1b83dbc
Eddy Lujan <eddylujann@gmail.com>
2023-03-09 19:57:01 +0100

    post loader

--- src/app/shared/components/feed/posts/posts.component.ts
+++ src/app/shared/components/feed/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { AuthenticationService } from "@core/http/authentication.service";
 import { Post } from "@data/models/post";
 import { UserService } from "@data/services/api/user.service";
@@ -13,21 +13,37 @@ export class PostsComponent implements OnInit {
       public url: string;
       @Input() posts: any;
       @Input() noMore: boolean = false;
-
       @Input() identity: any;
       @Output() eventViewMore = new EventEmitter();
       @Output() eventGotoProfile = new EventEmitter();
 
+      // Loader properties
+      @ViewChild('ElementRefCard') ElementViewCard: ElementRef;
+      @ViewChild('ElementRefCardBody') ElementViewCardBody: ElementRef;
+
       // TODO: Lifecycle
-      constructor(
-            private _userService: UserService,
-            private _authService: AuthenticationService
-      ) {
+      constructor(private _userService: UserService, private _authService: AuthenticationService) {
             this.url = this._userService.url;
             this.identity = this._authService.getIdentity();
       }
-
+      
       // TODO: Helpers
+      ngAfterViewInit() {
+            this.setPlaceholder();
+      }
+
+      public setPlaceholder() {
+            console.log(this.ElementViewCardBody.nativeElement);
+            if ( !this.ElementViewCard.nativeElement.classList.contains("card--skeleton") ) {
+                  console.log("Entra en el if");
+                  
+                  this.ElementViewCard.nativeElement.classList.add("card--skeleton");
+                  this.ElementViewCardBody.nativeElement.innerHTML = "";
+            } else {
+                  console.log("entra en el else");
+                  
+            }
+      }
       ngOnInit() {}
 
       viewMore() {
