29e86f4b4137b81e3b9866517b150d3f515337f0
Eddy <eddylujann@gmail.com>
2023-03-12 16:22:13 +0100

    Cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -17,41 +17,3 @@ export const environment = {
     appId: "1:637024328463:web:a12bd625935ce9fede3c7a"
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-=======
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  //uri: "http://localhost:3800/",
-
-  firebaseConfig: {
-    apiKey: "AIzaSyBQBTfe5rd1CzplaqIXRLtM44ngHjBbEPU",
-    authDomain: "instagram-e7380.firebaseapp.com",
-    databaseURL: "https://instagram-e7380-default-rtdb.firebaseio.com",
-    projectId: "instagram-e7380",
-    storageBucket: "instagram-e7380.appspot.com",
-    messagingSenderId: "637024328463",
-    appId: "1:637024328463:web:a12bd625935ce9fede3c7a"
-  }
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
->>>>>>> Stashed changes
