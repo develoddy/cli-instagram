eabd04c7f28e1faf724075df03510d33697e94a1
Eddy <eddylujann@gmail.com>
2023-03-25 11:43:55 +0100

    Update

--- src/app/modules/onboarding/login/login.component.ts
+++ src/app/modules/onboarding/login/login.component.ts
@@ -1,27 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '@core/http/authentication.service';
-import { User } from "@data/models/user";
+import { User } from '@data/models/user';
 import { UserService } from '@data/services/api/user.service';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent  implements OnInit  {
-
+export class LoginComponent implements OnInit {
   // TODO: Properties
 
   // TODO: Lifecycle
-  constructor( 
-    public authService: AuthenticationService
-  ) {}
+  constructor(public authService: AuthenticationService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // TODO: Helpers
-
-  
 }
