d4cc7c5011a386e6956b7a27f27c1b6160a94a0d
Eddy <eddylujann@gmail.com>
2023-03-29 20:43:28 +0200

    validar campos

--- src/app/modules/onboarding/accounts/accounts.component.ts
+++ src/app/modules/onboarding/accounts/accounts.component.ts
@@ -9,7 +9,6 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./accounts.component.css'],
 })
 export class AccountsComponent implements OnInit {
-
   public showBirthDay = false;
   public showAccount = true;
   public showCodePhone = false;
@@ -17,39 +16,49 @@ export class AccountsComponent implements OnInit {
   public birthDay: any; // OBTENER LOS DATOS DEL CUMPLEAÑOS DEL USUARIO
   public codePone: string;
 
-  constructor(public authService: AuthenticationService, private router: Router) {}
+  constructor(
+    public authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
+  // ESTA FUNCION TE REGRESARÁ A LA VISTA DE LOGUIN.
   public sigIn(data: any) {
     this.router.navigate(['login']);
   }
 
+  // ESTA FUNCION TE MOSTRARÁ LAS VISTA DE BIRTHDAY.
   public showBirthday(data: any) {
-    
+    // HAY QUE VALIDAD QUE LOS CAMPOS CONTIENEN DATOS
+
     this.showBirthDay = !this.showBirthDay;
     this.showAccount = !this.showAccount;
     this.user = data; // Objc
   }
 
+  // ESTA FUNCION TE MOSTRARÁ LA VISTA DE CODEPHONE.
   public showViewCodePhone(data: any) {
     this.showCodePhone = !this.showCodePhone;
-    this.showBirthDay = !this.showBirthDay;  
+    this.showBirthDay = !this.showBirthDay;
     this.birthDay = data; // Objc - 20 ene 2023
   }
 
-  public showToFeed( data: string ) {
-    this.codePone = data; // String
-  }
-
-  public backAccount(data:any) {
+  // ESTA FUNCIÓM TE REGRESARÁ A LA VISTA DE ACCOUNT.
+  public backAccount(data: any) {
     this.showAccount = true;
-    this.showBirthDay = false; 
+    this.showBirthDay = false;
   }
 
+  // ESTA FUNCIÓN TE REGRESARÁ A LA VISTA BIRTHDAY.
   public backBirthday(data: any) {
     this.showBirthDay = false;
-      this.showAccount = true;
-      this.showCodePhone = !this.showCodePhone;
+    this.showAccount = true;
+    this.showCodePhone = !this.showCodePhone;
+  }
+
+  // ESTA FUNCION TE REDIRGE A LA VISTA DEL FEED.
+  public showToFeed(data: string) {
+    this.codePone = data; // String
   }
 }
