2a41f62713c6b68e91ff0bc8742a7c64fe158263
Eddy Lujan <eddylujann@gmail.com>
2023-04-10 18:23:19 +0200

    uploader pst

--- src/app/shared/components/modals/modaladdpost/modaladdpost.component.ts
+++ src/app/shared/components/modals/modaladdpost/modaladdpost.component.ts
@@ -3,8 +3,6 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { BehaviorSubject, Observable, interval, timer } from 'rxjs';
 
 
-
-
 class ImageSnippet {
   constructor(public src: string, public file: File) {}
 }
@@ -16,103 +14,60 @@ class ImageSnippet {
 })
 export class ModaladdpostComponent implements OnInit  {
 
-  value = 100;
-  seconds: number = 0;
-
-  private file: File;
-  //file:any;
-  selectedFile: ImageSnippet;
-  sampleProfilePic = "../../../../../assets/images/profile-picture.png";
-
-  urls = new Array<string>();
+  //private file        : File;
+  file: any;
+  selectedFile        : ImageSnippet;
+  sampleProfilePic    = "../../../../../assets/images/profile-picture.png";
+  urls                = new Array<string>();
+  value               = 100;
+  seconds: number     = 0;
+  public spinner: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor() {}
 
-  ngOnInit(){
-    /*const time = 60;
-    const timer$ = interval(1000);
+  ngOnInit() {
 
-    const sub = timer$.subscribe(sec => {
-      this.value = 100 - (sec * 100) / 60;
-      this.seconds = sec;
-
-      if (this.seconds === 60) {
-        sub.unsubscribe();
-      }
-    });*/
-    this.startProgress();
-  }
-  
-
-  isOpen: boolean = false;
-
-  toggleExpansion() {
-    this.isOpen = !this.isOpen;
   }
 
-  startTimer(seconds: number) {}
-
-  progress = new BehaviorSubject(0);
-  startProgress() {
-    const interval = setInterval(() => {
-      this.progress.next(this.progress.value + 5);
-      if (this.progress.value >= 100) {
-        clearInterval(interval);
-      }
-    }, 500);
-  }
-
-  
-  
   public proccessFile(photo:any) {
     console.log(this.urls);
-    
     this.file = photo.files[0];
-
-    console.log(this.file);
-    
     const reader = new FileReader();
+
+    this.startProgress();
+    this.spinner.next(true);
     reader.addEventListener('load', (event: any) => {
+      this.spinner.next(false);
+      console.log("this.file" );
+      console.log(this.file);
       this.selectedFile = new ImageSnippet(event.target?.result, this.file);
       this.urls.push(event.target.result);
     });
     reader.readAsDataURL(this.file);
   }
 
-  // proccessFile(event:any) {
-  //   console.log(event);
-    
-  //   this.urls = [];
-  //   this.file = event.target.files;   
-  //     for (let file of this.file) {
-  //       let reader = new FileReader();
-  //       reader.onload = (e: any) => {
-  //         this.urls.push(e.target.result);
-  //       }
-  //       reader.readAsDataURL(file);
-  //     }
-  // }
-
-
-  deleteImage(): void {
-    this.selectedFile = new ImageSnippet('', this.file);
-    console.log("ArroLeft");
-    console.log(this.selectedFile);
-    console.log("this.urls.length : "+ this.urls.length );
-
-    if(this.urls.length === 1) {
+  public deleteImage(): void {
+    if(this.urls.length == 1) {
       this.urls = [];
-    }
+      console.log("this.file delete antes: " );
+      console.log(this.file);
+      this.file = null; //new File([], "", undefined);
 
-  
-    /*if(this.urls.length === 1) {
-      this.urls = [];
-      return;
+      console.log("this.file delete despues: " );
+      console.log(this.file);
+      this.selectedFile = new ImageSnippet('',this.file);
     }
-    this.urls = this.urls.filter((a) => a !== url);
-    console.log();*/
   }
 
-
-  
+  progress = new BehaviorSubject(0);
+  startProgress() {
+    this.spinner.next(true);
+    const interval = setInterval(() => {
+      this.spinner.next(false);
+      this.progress.next(this.progress.value + 100);
+      if (this.progress.value >= 100) {
+        clearInterval(interval);
+      }
+    }, 5);
+  }
 }
